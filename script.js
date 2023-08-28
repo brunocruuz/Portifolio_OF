cd69fd1386bf16012ab337da1c94e62e46171690
brunocruuz <bruno_cruz10@hotmail.com>
2023-08-27 22:36:22 -0300

    Att com JS e CSS

---
+++ script.js
@@ -0,0 +1,15 @@
+ 
+
+window.sr = ScrollReveal({reset: true})
+sr.reveal('.efeitoScrollReveal',{
+    rotate: {x:20, y:20, z:0},
+    duration: 1300}) /*duration é o tempo de duração da animação, 1000 é igual a 1 segundo */ 
+
+// sr.reveal('.efeitoScrollReveal_Efeito2',{
+//     rotate: {x:10, y:80, z:0},
+//     duration: 2000
+// })
+sr.reveal('.efeitoScrollReveal_Efeito2',{
+    rotate: {x:50, y:50, z:0},
+    duration: 1500
+})
